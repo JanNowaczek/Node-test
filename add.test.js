794b090ececae631746def335905ce8be53eaa0d
mrozo2m <mrozo2m@interia.pl>
2019-04-23 10:44:39 +0200

    dodawanie ujemnych liczb i liczb po przecinku test

--- add.test.js
+++ add.test.js
@@ -1,13 +1,16 @@
-const add = (a, b) => (Number(a) && Number(b)) ? Number(a) + Number(b) : 0;
+const add = (a, b) => (Number(a) && Number(b)) ? Math.round((Number(a) + Number(b))*1000)/1000 : 0;
+
 if (add(2, 5) !== 7
 || add(2,10) !== 12
-|| add(0,0) !== 0) {
+|| add(0,0) !== 0
+|| add(-10, -20) !== -30) {
     throw new Error('Błąd dodawania.')
 }
 console.log('Podstawowe dodawanie liczb całkowitych działa')
 
 if (add('2', '5') !== 7
-|| add('45', '125') !== 170) {
+|| add('45', '125') !== 170
+|| add('20', '-10') !== 10) {
     throw new Error('Błąd dodawania liczb w stringach')
 }
 console.log('Dodawanie liczb w stringach działa')
@@ -17,7 +20,7 @@ if (add('ala', 'ma kota') !== 0 || add('ala', 5) !== 0) {
 }
 console.log('Funkcja zwraca 0 dla literek')
 
-// if (add(0.2, 0.4) !== 0.6 || add(0.1, 0.01) !== 0.11) {
-    // throw new Error('Błąd dodawania liczb zmiennoprzecinkowych')
-// }
-// console.log('Dodawanie liczb zmiennoprzecinkowych działa')
+if (add(0.2, 0.4) !== 0.6 || add(0.1, 0.01) !== 0.11) {
+    throw new Error('Błąd dodawania liczb zmiennoprzecinkowych')
+}
+console.log('Dodawanie liczb zmiennoprzecinkowych działa do 3 miejsc po przecinku')
